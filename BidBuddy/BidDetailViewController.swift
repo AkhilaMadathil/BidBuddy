6b454d203614cb4c4a4698a1a187eb53186587af
Akhila Madathil <madathilakhila@gmail.com>
2019-01-05 16:36:02 +0530

    Bid detail view controller

---
+++ BidBuddy/BidDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BidDetailViewController.swift
+//  BidBuddy
+//
+//  Created by MAKL on 05/01/19.
+//  Copyright © 2019 Akhila. All rights reserved.
+//
+
+import UIKit
+
+class BidDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var liveStatus: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        flashBG()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func flashBG(){
+        UIView.animate(withDuration: 0.7, animations: {
+            self.view.backgroundColor = UIColor.red
+
+        })
+    }
+
+}
